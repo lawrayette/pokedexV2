cefc8789b12491405bfd12fa475ffb63d4e97e41
Karen Molina <milhadasdepapel@gmail.com>
2019-02-25 14:42:01 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+window.allPokemon = {
+
+  dataFiltered: (data, types) => {
+
+    const arrayFilter = data.filter(data => data.type.includes(types));
+    return arrayFilter;
+
+  },
+
+  orderData: (arrayOfType) => {
+
+    arrayOfType.sort((a, b) => a.name > b.name ? 1 : -1);
+    return arrayOfType;
+  },
+
+  compute: (candy) => {
+   
+  candy.reduce((prev, next) => prev + next);
+  return candy; 
+  }
+
+}
+
